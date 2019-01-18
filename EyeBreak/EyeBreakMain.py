7ecb95eefa545017b6ceefa175011cb932a5124b
K Wodzicki <krwodzicki@gmail.com>
2019-01-18 09:22:09 -0600

    Adds widget for each display
    This update creates a QLabel widget for each display so
    that whole computer goes black.
    
    To do:
      Move worker thread to QThread?
      Add progress bar to EyeBreakLabel widget
    
    v0.0.2

--- EyeBreak/EyeBreakMain.py
+++ EyeBreak/EyeBreakMain.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QLabel;
+from PyQt5.QtWidgets import QDesktopWidget, QLabel;
 from PyQt5.QtGui     import QFont;
 from PyQt5.QtCore    import pyqtSignal, pyqtSlot, Qt;
 from threading       import Thread, Event;
@@ -13,34 +13,71 @@ else:
   raise Exception('Not code/tested!')
 #  cmd = ['afplay', 'C:\\Windows\\Media\\notify.wav'];
 #  powershell -c (New-Object Media.SoundPlayer "C:\Windows\Media\notify.wav").PlaySync();
-class EyeBreakMain( QLabel ):
-  __showSig = pyqtSignal();
-  __hideSig = pyqtSignal();
-  def __init__(self, debug = False):
-    QLabel.__init__(self, 'Take a Break');
-    self.setAlignment( Qt.AlignCenter );
-    self.setFont( QFont('SansSerif', 48) );
-    self.setStyleSheet("background-color : black; color : gray;")
+
+class EyeBreakLabel( QLabel ):
+  showSig = pyqtSignal();                                       # Signal for showing window(s)
+  hideSig = pyqtSignal();                                       # Signal for hiding window(s)
+  def __init__(self, x, y, text = None):
+    '''
+    Inputs:
+       x  : x coordinate of label location
+       y  : y coordinate of label location
+    Keywords:
+       text  : Text displayed on window.
+       debug : Set for faster switching time
+    '''
+    QLabel.__init__(self, 
+      'Take a Break' if text is None else text
+    );                                                              # Initialize super class
+    self.setAlignment( Qt.AlignCenter );                            # Align text to middle of screen
+    self.setFont( QFont('SansSerif', 48) );                         # Set font an font size
+    self.setStyleSheet("background-color : black; color : gray;");  # Set background to black and text color to gray
+    self.move( x, y );                                              # Move label to given screen
+    
+    self.showSig.connect(self.__show);                            # Connect __showSig signal to the __show method
+    self.hideSig.connect(self.__hide);                            # Connect __hideSig signal to the __hide method
+  ##########################################
+  @pyqtSlot()
+  def __show(self, *args):
+    self.showFullScreen();
+  ##########################################
+  @pyqtSlot()
+  def __hide(self, *args):
+    self.hide();
+
+class EyeBreakMain( QDesktopWidget ):
+  labels = [];
+  def __init__(self, text = None, debug = False):
+    '''
+    Keywords:
+       text  : Text displayed on window.
+       debug : Set for faster switching time
+    '''
+    QDesktopWidget.__init__(self);
+
+    signal.signal(signal.SIGINT,  self.__exit_gracefully);          # Set method to run on interupt signal
+    signal.signal(signal.SIGTERM, self.__exit_gracefully);          # Set method to run on kill signal
+
+    self.__text    = text;
     self.__debug   = debug;
-    self.__running = Event();
-    signal.signal(signal.SIGINT,  self.__exit_gracefully)
-    signal.signal(signal.SIGTERM, self.__exit_gracefully)
-    
-    self.__showSig.connect(self.__show)
-    self.__hideSig.connect(self.__hide)
-    
+    self.__running = Event();                                       # Initialize threading event
+    self.__visible = False;
+
+    self.add_screens();
     self.thread    = Thread(target = self.__thread);
     self.thread.start();
-
-  @pyqtSlot()
-  def __show(self):
-  	self.showFullScreen();
-  @pyqtSlot()
-  def __hide(self):
-  	self.hide();
-  
+  #####################################################
+  def add_screens(self):
+    '''
+    Method for creating EyeBreakLabel for each screen
+    '''
+    for i in range( self.screenCount() ):          # Iterate over the number of screens
+      x, y, height, width = self.availableGeometry(i).getRect();
+      self.labels.append( EyeBreakLabel(x, y, text = self.__text ) );
+  #######################################################  
   def __exit_gracefully(self, *args):
     self.__running.clear();
+  #######################################################
   def __thread(self):
     self.__running.set();                                 # Set the event on thread start
     if self.__debug:                                      # If debug was set
@@ -51,12 +88,16 @@ class EyeBreakMain( QLabel ):
     t0 = time.time();                                     # Get current time
     while self.__running.is_set():                        # While the __running event is set
       if (time.time()-t0) >= delay[i]:                    # If difference between current time and t0 >= delay time
-        if self.isVisible():                              # If the window is currently visible
-          self.__hideSig.emit();                          # Hide the window
+        if self.__visible:                                # If the window is currently visible
+          for label in self.labels :
+            label.hideSig.emit();                         # Hide the window
         else:                                             # Else, it is hidden so
-          self.__showSig.emit();                          # Show the window
+          for label in self.labels :
+            label.showSig.emit();                         # Hide the window
+        self.__visible = not self.__visible;
         Popen( cmd, stdout = DEVNULL, stderr = STDOUT );  # Play notification sound
         t0 = time.time();                                 # Update t0
         i  = (i + 1) % 2;                                 # Increment i ensuring it is always either 0 or 1
       time.sleep(0.1);                                    # Sleep for 100 ms
+    for label in self.labels: label.close();
     self.close();                                         # Close the window at the end of the thread
