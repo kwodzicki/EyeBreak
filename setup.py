0d13fb3b5ba40fff0b52ce2992cb0a03ed4492f1
K Wodzicki <krwodzicki@gmail.com>
2019-01-24 13:20:48 -0600

    1st auto install OS X attempt
    This version is the first attempt at getting
    the application to 'auto build' on install
    to OS x.
    
    v0.0.3

--- setup.py
+++ setup.py
@@ -1,16 +1,53 @@
 #!/usr/bin/env python
-from setuptools import setup
-import setuptools
+import os, sys, shutil;
+from subprocess import Popen;
+from setuptools import setup;
+import setuptools;
+
+# Set up app name and other info
+name    = "20-20-20 EyeBreak";
+script  = "bin/EyeBreak";
+require = ["PyQt5"]
+if sys.platform == 'darwin': require.append( 'pyinstaller' );
+
+def darwin_install():
+  topdir = os.path.dirname(os.path.realpath(__file__));
+  blddir = "/tmp/2020_build";
+  wrkdir = os.path.join( blddir, "work" );
+  dstdir = os.path.join( blddir, "dist" );
+  icndir = os.path.join( topdir, "icons" );
+  appdir = os.path.join( os.path.expanduser("~"), "Applications" );
+  src    = os.path.join( dstdir, "{}.app".format(name) );
+  dst    = os.path.join( appdir, "{}.app".foramt(name) );
+  if not os.path.isdir(wrkdir): os.makedirs( wrkdir );
+  if not os.path.isdir(dstdir): os.makedirs( dstdir );
+  cmd = ["pyinstaller", "-ywF",
+         "--distpath", dstdir,
+         "--workpath", wrkdir,
+         "--specpath", blddir,
+         "--name",     name,
+         "-i",         icon,
+         script]
+  proc = Popen( cmd );
+  proc.communicate();
+  if os.path.isdir( dst ): shutil.rmtree( dst );
+  shutil.move( src, appdir );
+  shutil.rmtree( blddir );
+
+
+# Move the app into place in the user's application directory
 
 setuptools.setup(
-  name         = "EyeBreak",
-  description  = "A GUI program that reminds you to take an eye break",
-  url          = "https://github.com/kwodzicki/makemkv_to_mp4",
-  author       = "Kyle R. Wodzicki",
-  author_email = "krwodzicki@gmail.com",
-  version      = "0.0.2b",
-  packages     = setuptools.find_packages(),
-  install_requires = [ "PyQt5" ],
-  scripts=['bin/EyeBreak'],
-  zip_save = False,
+  name             = name,
+  description      = "A GUI program that reminds you to take an eye break",
+  url              = "https://github.com/kwodzicki/EyeBreak",
+  author           = "Kyle R. Wodzicki",
+  author_email     = "krwodzicki@gmail.com",
+  version          = "0.0.3",
+  packages         = setuptools.find_packages(),
+  install_requires = require,
+  scripts          = [script],
+  zip_save         = False,
 );
+
+if sys.platform == 'darwin': darwin_install();
